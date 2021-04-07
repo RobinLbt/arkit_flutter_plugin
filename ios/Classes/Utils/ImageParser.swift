f201e18568c918dee456eb0d16a93839de3d05ff
RobinLbt <supremedub@gmail.com>
2021-04-07 14:31:19 +0200

    dynamic images

--- ios/Classes/Utils/ImageParser.swift
+++ ios/Classes/Utils/ImageParser.swift
@@ -1,17 +1,19 @@
 import Foundation
 
 func getImageByName(_ name: String) -> UIImage? {
-    if let img = UIImage(named: name) {
-        return img
-    }
-    if let path = Bundle.main.path(forResource: SwiftArkitPlugin.registrar!.lookupKey(forAsset: name), ofType: nil) {
-        return UIImage(named: path)
-    }
-    do {
-        let data = try Data.init(contentsOf: URL.init(string: name)!)
-        return UIImage(data: data)
-    } catch {
-        
-    }
-    return nil
+       if let path = Bundle.main.path(forResource: SwiftArkitPlugin.registrar!.lookupKey(forAsset: name), ofType: nil) {
+         return UIImage(named: path)
+     }
+     if let url = URL.init(string: name) {
+       do {
+         let data = try Data.init(contentsOf: url)
+         return UIImage(data: data)
+       } catch {
+
+       }
+     }
+     if let base64 = Data(base64Encoded: name, options: .ignoreUnknownCharacters) {
+         return UIImage(data: base64)
+     }
+     return nil
 }
